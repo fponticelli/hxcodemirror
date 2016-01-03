edd1bedb87c6cfa8b9989d3d0ae5b982fcf4da2d
Franco Ponticelli <franco.ponticelli@gmail.com>
2016-01-03 16:15:41 -0700

    added static constructor fromTextArea

--- src/codemirror/CodeMirror.hx
+++ src/codemirror/CodeMirror.hx
@@ -1,9 +1,12 @@
 package codemirror;
 
 import js.html.Element;
+import js.html.TextAreaElement;
 
 @:native("CodeMirror")
 extern class CodeMirror {
+  public static function fromTextArea(el : TextAreaElement, ?options : Options) : CodeMirror;
+
   @:override(function(callback : Element -> Void, ?options : Options) : Void {})
   public function new(el : Element, ?options : Options) : Void;
 }
