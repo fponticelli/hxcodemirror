819af81af0491abb97ae2bb5f498c44f482ff103
Franco Ponticelli <franco.ponticelli@gmail.com>
2016-01-03 16:43:16 -0700

    events for LineWidget

--- src/codemirror/LineWidget.hx
+++ src/codemirror/LineWidget.hx
@@ -2,5 +2,12 @@ package codemirror;
 
 @:native("CodeMirror.LineWidget")
 extern class LineWidget extends EventEmitter {
-
+  // EVENTS
+  /*
+  Fired whenever the editor re-adds the widget to the DOM. This will happen once right after the widget is added (if it is scrolled into view), and then again whenever it is scrolled out of view and back in again, or when changes to the editor options or the line the widget is on require the widget to be redrawn.
+  */
+  inline function onRedraw(handler : Void -> Void) : Void
+    this.on("redraw", handler);
+  inline function offRedraw(handler : Void -> Void) : Void
+    this.off("redraw", handler);
 }
