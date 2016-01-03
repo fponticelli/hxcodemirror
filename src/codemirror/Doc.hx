41926bbccbc762aa75a40bbf33c3b7f803e1cbb6
Franco Ponticelli <franco.ponticelli@gmail.com>
2016-01-03 16:41:35 -0700

    refined event aliases

--- src/codemirror/Doc.hx
+++ src/codemirror/Doc.hx
@@ -8,17 +8,29 @@ extern class Doc extends EventEmitter {
   /**
   Fired whenever a change occurs to the document. changeObj has a similar type as the object passed to the editor's "change" event.
   */
-  function onChange(handler : Doc -> ChangeObject -> Void) : Void;
+  inline function onChange(handler : Doc -> ChangeObject -> Void) : Void
+    this.on("change", handler);
+  inline function offChange(handler : Doc -> ChangeObject -> Void) : Void
+    this.off("change", handler);
   /**
   See the description of the same event on editor instances.
   */
-  function onBeforeChange(handler : Doc -> ChangeObject -> Void) : Void;
+  inline function onBeforeChange(handler : Doc -> ChangeObject -> Void) : Void
+    this.on("beforeChange", handler);
+  inline function offBeforeChange(handler : Doc -> ChangeObject -> Void) : Void
+    this.off("beforeChange", handler);
   /**
   Fired whenever the cursor or selection in this document changes.
   */
-  function onCursorActivity(handler : Doc -> Void) : Void;
+  inline function onCursorActivity(handler : Doc -> Void) : Void
+    this.on("cursorActivity", handler);
+  inline function offCursorActivity(handler : Doc -> Void) : Void
+    this.off("cursorActivity", handler);
   /**
   Equivalent to the event by the same name as fired on editor instances.
   */
-  function onBeforeSelectionChange(handler : Doc -> { ranges : Array<Range>, origin : String, update : Array<Range> -> Void } -> Void) : Void;
+  inline function onBeforeSelectionChange(handler : Doc -> { ranges : Array<Range>, origin : String, update : Array<Range> -> Void } -> Void) : Void
+    this.on("beforeSelectionChange", handler);
+  inline function offBeforeSelectionChange(handler : Doc -> { ranges : Array<Range>, origin : String, update : Array<Range> -> Void } -> Void) : Void
+    this.off("beforeSelectionChange", handler);
 }
--- src/codemirror/LineHandle.hx
+++ src/codemirror/LineHandle.hx
@@ -1,12 +1,21 @@
 package codemirror;
 
-extern class LineHandle {
+extern class LineHandle extends EventEmitter {
+
+  // EVENTS
+
   /**
   Will be fired when the line object is deleted. A line object is associated with the start of the line. Mostly useful when you need to find out when your gutter markers on a given line are removed.
   */
-  function onDelete(handler : Void -> Void) : Void;
+  inline function onDelete(handler : Void -> Void) : Void
+    this.on("delete", handler);
+  inline function offDelete(handler : Void -> Void) : Void
+    this.off("delete", handler);
   /**
   Fires when the line's text content is changed in any way (but the line is not deleted outright). The change object is similar to the one passed to change event on the editor object.
   */
-  function onChange(handler : LineHandle -> ChangeObject -> Void) : Void;
+  inline function onChange(handler : LineHandle -> ChangeObject -> Void) : Void
+    this.on("change", handler);
+  inline function offChange(handler : LineHandle -> ChangeObject -> Void) : Void
+    this.off("change", handler);
 }
