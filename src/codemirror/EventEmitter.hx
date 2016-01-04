96a5dde263c87dc4a4ae23ca234102ca2e30c6fd
Franco Ponticelli <franco.ponticelli@gmail.com>
2016-01-03 18:36:44 -0700

    docs for event emitter

--- src/codemirror/EventEmitter.hx
+++ src/codemirror/EventEmitter.hx
@@ -4,7 +4,13 @@ import haxe.Constraints;
 import haxe.extern.Rest;
 
 extern class EventEmitter {
+  /**
+  Register an event handler for the given event type (a String) on the editor instance. There is also a CodeMirror.on(object, type, func) version that allows registering of events on any object.
+  */
   public function on(event : String, callback : Function) : Void;
+  /**
+  Remove an event handler on the editor instance. An equivalent CodeMirror.off(object, type, func) also exists.
+  */
   public function off(event : String, callback : Function) : Void;
   public function signal(target : Dynamic, name : String, args : Rest<Dynamic>) : Void;
 }
